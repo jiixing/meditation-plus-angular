db68d8500ae23a4860dd65a01db3131db8d6ad54
jiixing <jiixing@yahoo.com>
2017-06-14 20:06:19 -0700

    testing submitting message calling MessageService

--- src/app/message/message.component.spec.ts
+++ src/app/message/message.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {fakeAsync, async, tick, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MessageComponent} from './message.component';
 import {FakeMessageService} from './testing/fake-message.service';
@@ -12,6 +12,7 @@ import {FormsModule} from '@angular/forms';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MaterialModule} from '@angular/material';
+import {TestHelper} from '../../testing/test.helper'
 
 describe('MessageComponent', () => {
   let component: MessageComponent;
@@ -45,12 +46,52 @@ describe('MessageComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
     const compiled = fixture.debugElement.nativeElement;
-    expect(
-      compiled.querySelectorAll('.message-row').length
-    ).toBe(3);
 
     expect(
       compiled.querySelector('button[type="submit"]').disabled
     ).toBe(true);
   });
+
+  it('should have 3 messages', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(
+      compiled.querySelectorAll('.message-row').length
+    ).toBe(3);
+  })
+
+  it('should have disabled submit button', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(
+      compiled.querySelector('button[type="submit"]').disabled
+    ).toBe(true);
+
+  })
+
+  it('should send message to service', () => {
+    const compiled = fixture.debugElement.nativeElement;
+
+    let mockService =
+      fixture.debugElement.injector.get<any>(MessageService) as FakeMessageService;
+    let spy = spyOn(mockService, 'post').and.callThrough();
+
+    let input = compiled.querySelector('textarea');
+    const test_message = '##HELLO_GOODBYE##'
+    input.value = test_message
+
+    TestHelper.sendInputs(fixture, [input]).then(() => {
+      expect(
+        compiled.querySelector('button[type="submit"]').disabled
+      ).toBe(false);  // expect user can click submit button
+
+      // simulate submit
+      let form = compiled.querySelector('form');
+      TestHelper.dispatchEvent(form, 'submit');
+
+      // the service function is called with currentMessage
+      expect(spy.calls.count()).toBe(1);
+      expect(spy.calls.argsFor(0)).toEqual([test_message])
+    })
+
+
+  })
 });
--- src/testing/test.helper.ts
+++ src/testing/test.helper.ts
@@ -1,6 +1,13 @@
-import { Response, ResponseOptions } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import {Response, ResponseOptions} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import {DebugElement} from "@angular/core";
+import {ComponentFixture} from '@angular/core/testing';
 
+function createEvent(eventType: any): Event {
+  const evt: Event = document.createEvent('Event');
+  evt.initEvent(eventType, true, true);
+  return evt;
+}
 export class TestHelper {
   public static fakeResponse(json: {} = {}) {
     return Observable.create(obs => {
@@ -16,6 +23,22 @@ export class TestHelper {
   }
 
   public static noRespose() {
-    return Observable.create(() => {});
+    return Observable.create(() => {
+    });
   }
+
+
+  public static dispatchEvent(element: any, eventType: any) {
+    element.dispatchEvent(createEvent(eventType));
+  }
+
+  public static sendInputs(fixture: ComponentFixture<any>, inputs: Array<any>)
+  // inputs has to be native element, useful to test forms
+  {
+    inputs.forEach(input => {
+      this.dispatchEvent(input, 'input');
+    });
+    fixture.detectChanges();
+    return fixture.whenStable();
+  };
 }
