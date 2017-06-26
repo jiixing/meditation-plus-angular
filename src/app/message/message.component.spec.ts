96f7f0576afa2a5a946de86f4d3e504a40595286
jiixing <jiixing@yahoo.com>
2017-06-25 23:45:47 -0700

    fix failing test

--- src/app/message/message.component.spec.ts
+++ src/app/message/message.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, fakeAsync, tick, flushMicrotasks } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MessageComponent } from './message.component';
 import { FakeMessageService } from './testing/fake-message.service';
@@ -63,31 +63,28 @@ describe('MessageComponent', () => {
       compiled.querySelector('button[type="submit"]').disabled
     ).toBe(true);
   });
-  describe("test input", () => {
-    it('should send message to service', (done) => {
-      const compiled = fixture.debugElement.nativeElement;
+  it('should send message to service', (done) => {
+    const compiled = fixture.debugElement.nativeElement;
 
-      const mockService =
-        fixture.debugElement.injector.get<any>(MessageService) as FakeMessageService;
-      const spy = spyOn(mockService, 'post').and.callThrough();
+    const mockService =
+      fixture.debugElement.injector.get<any>(MessageService) as FakeMessageService;
+    const spy = spyOn(mockService, 'post').and.callThrough();
 
-      const input = compiled.querySelector('textarea');
-      input.value = '##HELLO_GOODBYE##';
 
-      TestHelper.sendInputs(fixture, [input]).then(() => {
-        expect(
-          compiled.querySelector('button[type="submit"]').disabled
-        ).toBe(false);  // expect user can click submit button
+    component.currentMessage = '##HELLO_GOODBYE##';
+    TestHelper.advance(fixture).then(() => {
+      expect(
+        compiled.querySelector('button[type="submit"]').disabled
+      ).toBe(false);  // expect user can click submit button
 
-        // simulate submit
-        const form = compiled.querySelector('form');
-        TestHelper.dispatchEvent(form, 'submit');
+      // simulate submit
+      const form = compiled.querySelector('form');
+      TestHelper.dispatchEvent(form, 'submit');
 
-        // the service function is called with currentMessage
-        expect(spy.calls.count()).toBe(1);
-        expect(spy.calls.argsFor(0)).toEqual(['##HELLO_GOODBYE##']);
-        done()
-      });
+      // the service function is called with currentMessage
+      expect(spy.calls.count()).toBe(1);
+      expect(spy.calls.argsFor(0)).toEqual(['##HELLO_GOODBYE##']);
+      done();
     });
   });
 
@@ -129,7 +126,6 @@ describe('MessageComponent', () => {
     component.messageHandler(websocket_data);
 
     TestHelper.advance(fixture).then(() => {
-      debugger
       // using By, can access custom tag directly
       expect(fixture.debugElement.queryAll(By.css('message-list-entry')).length).toBe(4);
 
@@ -143,7 +139,7 @@ describe('MessageComponent', () => {
       for (const entry of [1, 2, 3, 4]) {
         expect(component.messages[entry - 1].text).toEqual(`MyMessage ${entry}`);
       }
-      done()
+      done();
     });
 
   });
